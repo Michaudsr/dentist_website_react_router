f11ffe9357a656f3b3d95c951f0ee35252755021
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-08-28 13:59:30 -0400

    [contact, procedures] add contact and Procedures components

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+
+class Contact extends Component {
+    render() {
+
+        return (
+            <div>
+                <h1>Contact Us</h1>
+                <p>Steven Michaud, MD</p>
+                <p>Email: steven.michaud@dentist.com</p>
+                <p>Phone: 111-111-1111</p>
+            </div>
+        )
+    }
+    }
+    export default Contact;
